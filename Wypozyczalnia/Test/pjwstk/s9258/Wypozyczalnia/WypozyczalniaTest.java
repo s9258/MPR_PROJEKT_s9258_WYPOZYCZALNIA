651c52c4ce7dffd31c15aab9cb4ed73d1eeafbed
krzych <krzych@krzych-Komputer>
2012-11-27 20:09:29 +0100

    uzupelnienie kodu, dalsze testy

--- Wypozyczalnia/Test/pjwstk/s9258/Wypozyczalnia/WypozyczalniaTest.java
+++ Wypozyczalnia/Test/pjwstk/s9258/Wypozyczalnia/WypozyczalniaTest.java
@@ -1,11 +1,24 @@
 package pjwstk.s9258.Wypozyczalnia;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.Before;
 
 
 
 public class WypozyczalniaTest {
 
+		
+	Wypozyczalnia wypozyczalnia;
+	
+	@Before
+	
+	public void setUp () throws Exception
+	
+	{
+		
+		this.wypozyczalnia = new Wypozyczalnia();
+		
+	}
 	
 	
 @Test
@@ -25,6 +38,48 @@ Assert.assertEquals(3, kajak1.getczas());
 Assert.assertEquals(4, kajak1.getsztuk());
 
 }
+
+
+@Test
+public void testWypozyczKajak_2(){
+
+
+Wypozyczalnia wypozyczalnia = new Wypozyczalnia();
+
+
+Kajak_2 kajak2 = wypozyczalnia.WypozyczKajak_2(2, 3, 4);
+
+
+
+Assert.assertEquals("Kajak_2", kajak2.getTyp());
+Assert.assertEquals(2, kajak2.getcena());
+Assert.assertEquals(3, kajak2.getczas());
+Assert.assertEquals(4, kajak2.getsztuk());
+
+}
+
+
+@Test
+public void testWypozyczRowerWodny(){
+
+
+Wypozyczalnia wypozyczalnia = new Wypozyczalnia();
+
+
+RowerWodny rowerwodny = wypozyczalnia.WypozyczRowerWodny(2, 3, 4);
+
+
+
+Assert.assertEquals("RowerWodny", rowerwodny.getTyp());
+Assert.assertEquals(2, rowerwodny.getcena());
+Assert.assertEquals(3, rowerwodny.getczas());
+Assert.assertEquals(4, rowerwodny.getsztuk());
+
+
+
+}
+
+
 }
 
 
--- Wypozyczalnia/src/pjwstk/s9258/Wypozyczalnia/Wypozyczalnia.java
+++ Wypozyczalnia/src/pjwstk/s9258/Wypozyczalnia/Wypozyczalnia.java
@@ -12,5 +12,33 @@ public Kajak_1 WypozyczKajak
 	return new Kajak_1(cena, czas, sztuk);
 
 }
+
+public Kajak_2 WypozyczKajak_2 
+
+(int cena, int czas, int sztuk)
+
+{
+
+	return new Kajak_2(cena, czas, sztuk);
+
+}
+
+
+
+public RowerWodny WypozyczRowerWodny 
+
+(int cena, int czas, int sztuk)
+
+{
+
+	return new RowerWodny(cena, czas, sztuk);
+
+}
+
+
+
+
+
+
 }
 
